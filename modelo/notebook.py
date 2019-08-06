d29a04aaf544d3e0129106a1b15b7cead057c3bf
marina012 <marinalopezmurcia@gmail.com>
2019-08-06 08:44:48 +0000

    subo el nuevo modelo

---
+++ modelo/notebook.py
@@ -0,0 +1,239 @@
+#import APIs
+import pandas as pd
+import matplotlib
+import matplotlib.pyplot as plt
+import datetime
+from datetime import datetime
+from dateutil import parser
+import numpy as np
+import os, shutil
+import requests
+import json
+from netCDF4 import Dataset
+
+#import satellite submodules
+from wq_modules import sentinel
+from wq_modules import landsat
+from wq_modules import water
+from wq_modules import clouds
+from wq_modules import modeling_file
+
+#import meteo submodules
+from wq_modules import meteo
+
+#import general submodules
+from wq_modules import utils
+from wq_modules import config
+
+#widget
+import ipywidgets as widgets
+from ipywidgets import HBox, VBox, Layout
+from IPython.display import display
+from IPython.display import clear_output
+
+import re
+
+
+
+#Model visualization
+
+ruta= "/home/jovyan/datasets"
+
+def nombre_modelo(nombre):
+    return re.findall(r'/home/jovyan/datasets/([A-Za-z0-9\_\-]*)',nombre)[0]
+
+def busca_modelos(ruta):
+    files= [os.path.abspath(arch.path) for arch in os.scandir(ruta) if arch.is_file()]
+    lstFiles = []
+    for fichero in files:
+        (nombreFichero, extension) = os.path.splitext(fichero)
+        if(extension == ".nc"):
+            lstFiles.append(nombre_modelo(nombreFichero))
+            #print (nombreFichero+extension)
+    return(lstFiles)
+
+opt = busca_modelos(ruta)
+
+selection = widgets.Select(
+    options=opt,
+    value=opt[0],
+    # rows=10,
+    description='Models',
+    disabled=False,
+    layout=Layout(width='75%')
+)
+
+depth_wid = widgets.IntSlider(
+    value=7,
+    min=0,
+    max=35,
+    step=1,
+    description='Layer (depth):',
+    disabled=False,
+    continuous_update=False,
+    orientation='horizontal',
+    readout=True,
+    readout_format='d'
+)
+button3 = widgets.Button(
+    description='Show model output',
+)
+
+out3 = widgets.Output()
+
+@button3.on_click
+def model_on_click(b):
+    global dataset, lista, propiedades
+    nombre_dataset= ruta+"/"+selection.value+".nc"
+    dataset= Dataset(nombre_dataset, 'r', format='NETCDF4_CLASSIC')
+    lista=[[],[],[]]
+    propiedades=[[],[],[],[],[]]
+    carga_variables()
+    set_display()
+    with out3:
+        clear_output()
+        propiedades[0]=0
+        propiedades[1]=drop_date.value
+        propiedades[2]=depth_wid.value
+        propiedades[3]= None
+        propiedades[4]= None
+        actualiza_layout()
+
+
+            
+            
+vbox3 = VBox(children=[selection,button3,out3])
+vbox1 = VBox()
+
+
+
+def carga_variables():
+    global drop_var, lista, time
+    for n in dataset.variables.keys():
+        if n.find("time") >= 0:
+            time=n
+        dimensiones=''
+        for i in dataset.variables[n].dimensions:
+            dimensiones= dimensiones+" "+i
+        
+        dim=len(dataset.variables[n].dimensions)
+        if dim > 2 and dim <5:
+            lista[0]= np.append(lista[0],n)
+            lista[1]= np.append(lista[1],dim)
+            lista[2]= np.append(lista[2],dimensiones)
+
+def set_display():
+    global drop_var, drop_date, depth_wid, hb_3d, hb_2d
+    drop_var=widgets.Dropdown(
+        options=[(lista[0][n], n) for n in range(len(lista[0]))],
+        value=0,
+        description='Variables:',
+    )
+    date= set_date()
+    drop_date=widgets.Dropdown(
+        options=[(str(date[i]), i) for i in range(len(date))],
+        value=0,
+        description='Fecha:',
+    )
+    drop_date.observe(date_on_change, names='value')
+    drop_var.observe(variable_on_change, names='value')
+    hb_3d= HBox([drop_var, drop_date, depth_wid])
+    hb_2d= HBox([drop_var, drop_date])
+
+    
+def set_date():
+    date=[]
+    for i in range(len(dataset.variables[time])-1):
+        date=np.append(date, humanize_time(dataset.variables[time][i]))
+    return date
+
+def humanize_time(secs):
+    mins, secs = divmod(secs, 60)
+    hours, mins = divmod(mins, 60)
+    return '%02d:%02d:%02d' % (hours, mins, secs)  
+
+
+
+
+def actualiza_layout():
+
+    clear()
+    if lista[1][propiedades[0]]==4:
+        display(hb_3d)
+        aux=dataset.variables[lista[0][propiedades[0]]][propiedades[1],propiedades[2],:,:]
+        
+    if lista[1][propiedades[0]]==3:
+        display(hb_2d)
+        aux=dataset.variables[lista[0][propiedades[0]]][propiedades[1],:,:]
+        
+    v_m= np.amin(aux[:])
+    if v_m != np.nan and v_m < 0:
+        print(v_m)
+        aux[ aux==v_m ] = np.nan
+        
+    #aux[ aux==0 ] = np.nan
+    fig= plt.figure()
+    fig.add_subplot()
+    cmap = matplotlib.cm.jet
+    cmap.set_bad('grey',1.)
+
+    plt.imshow(np.transpose(aux), interpolation='nearest', vmin= propiedades[3], vmax= propiedades[4],cmap=cmap)
+    plt.colorbar()
+    plt.title(lista[0][propiedades[0]])
+    plt.ylabel("Latitude")
+    plt.xlabel("Longitude")
+    plt.show()
+    
+    #cid = fig.canvas.mpl_connect('button_press_event', onclick)
+    
+def onclick(event):
+    global valor_x, valor_y
+    x=int(event.xdata)
+    y=int(event.ydata)
+    print(x,y)
+    
+def clear():
+    clear_output()
+    plt.close()
+    
+    
+def variable_on_change(v):
+    propiedades[0]=v['new']
+    propiedades[3]= None
+    propiedades[4]= None
+    calcula_min_max()
+    actualiza_layout()
+    
+    
+def calcula_min_max():
+    var= dataset.variables[lista[0][propiedades[0]]][:]
+    v_max= np.amax(var[:])
+    v_m= np.amin(var[:])
+
+    var[ var==v_m ] = v_max+10
+    v_min= np.amin(var[:])
+
+    var[ var==v_max+10 ] = v_m
+
+    propiedades[3]=v_min
+    propiedades[4]=v_max
+
+def slider_on_change(v):
+    
+    propiedades[2]=v['new']
+    actualiza_layout()
+    
+depth_wid.observe(slider_on_change, names='value')
+
+
+def date_on_change(v):
+    propiedades[1]=v['new']
+    actualiza_layout()
+    
+
+    
+#Menu
+menu = widgets.Tab()
+menu.children = [vbox1, vbox3]
+menu.set_title(0,'Ajustes')
+menu.set_title(1,'Model visualization')
