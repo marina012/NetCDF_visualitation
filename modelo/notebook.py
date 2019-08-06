51e656491e1baf07e8bf3ad1f2bc800b48b64bbd
marina012 <marinalopezmurcia@gmail.com>
2019-08-06 11:46:54 +0000

    añado descripcion, unidades leyenda, cambio ejes ev prof

--- modelo/notebook.py
+++ modelo/notebook.py
@@ -81,6 +81,9 @@ button3 = widgets.Button(
 
 out3 = widgets.Output()
 
+
+
+
 @button3.on_click
 def model_on_click(b):
     global dataset, lista, propiedades
@@ -123,7 +126,7 @@ def carga_variables():
             lista[2]= np.append(lista[2],dimensiones)
 
 def set_display():
-    global drop_var, drop_date, depth_wid, hb_3d, hb_2d
+    global drop_var, drop_date, depth_wid, hb_3d, hb_2d, vb_ev_2d, vb_ev_3d, valor_x, valor_y, date
     drop_var=widgets.Dropdown(
         options=[(lista[0][n], n) for n in range(len(lista[0]))],
         value=0,
@@ -139,6 +142,42 @@ def set_display():
     drop_var.observe(variable_on_change, names='value')
     hb_3d= HBox([drop_var, drop_date, depth_wid])
     hb_2d= HBox([drop_var, drop_date])
+    
+    
+    valor_x= widgets.BoundedFloatText(
+    value=0,
+    min=0,
+    max=94,
+    step=1,
+    description='x:'
+    )
+    valor_y= widgets.BoundedFloatText(
+        value=0,
+        min=0,
+        max=121,
+        step=1,
+        description='y:'
+    )
+
+    boton_tiempo= widgets.Button(
+        description='Time'
+    )
+
+    boton_prof= widgets.Button(
+        description='Depth'
+    )
+
+    Label_cor= widgets.Label("Click on the map to choose the coordinates of which to see the evolution:")
+
+    vb_ev_text= VBox([valor_x, valor_y])
+    vb_ev_bot= VBox([boton_tiempo, boton_prof])
+    hb_ev_3d= HBox([vb_ev_text, vb_ev_bot, depth_wid])
+    hb_ev_2d= HBox([vb_ev_text, boton_tiempo])
+    vb_ev_3d= VBox([Label_cor, hb_ev_3d])
+    vb_ev_2d= VBox([Label_cor, hb_ev_2d])
+    
+    boton_prof.on_click(on_button_clicked_ev_prof)
+    boton_tiempo.on_click(on_button_clicked_ev_time)
 
     
 def set_date():
@@ -158,17 +197,30 @@ def humanize_time(secs):
 def actualiza_layout():
 
     clear()
+    des=""
+    try:
+        des=lista[0][propiedades[0]]+": "+dataset.variables[lista[0][propiedades[0]]].long_name
+    except:
+        des="Variable sin descripción"
+        
+    label= widgets.Label(des)
     if lista[1][propiedades[0]]==4:
-        display(hb_3d)
+        display(hb_3d, label)
         aux=dataset.variables[lista[0][propiedades[0]]][propiedades[1],propiedades[2],:,:]
+        ev=vb_ev_3d
         
     if lista[1][propiedades[0]]==3:
-        display(hb_2d)
+        display(hb_2d, label)
         aux=dataset.variables[lista[0][propiedades[0]]][propiedades[1],:,:]
+        ev=vb_ev_2d
         
+    try:
+        widgets.Label(lista[0][propiedades[0]],": ",dataset.variables[lista[0][propiedades[0]]].long_name)
+    except:
+        widgets.Label("Variable sin descripción")
+    
     v_m= np.amin(aux[:])
     if v_m != np.nan and v_m < 0:
-        print(v_m)
         aux[ aux==v_m ] = np.nan
         
     #aux[ aux==0 ] = np.nan
@@ -178,19 +230,24 @@ def actualiza_layout():
     cmap.set_bad('grey',1.)
 
     plt.imshow(np.transpose(aux), interpolation='nearest', vmin= propiedades[3], vmax= propiedades[4],cmap=cmap)
-    plt.colorbar()
+    cbar=plt.colorbar()
+    try:
+        cbar.set_label(dataset.variables[lista[0][propiedades[0]]].units)
+    except:
+        cbar.set_label("Unidades no especificadas")
     plt.title(lista[0][propiedades[0]])
     plt.ylabel("Latitude")
     plt.xlabel("Longitude")
     plt.show()
     
-    #cid = fig.canvas.mpl_connect('button_press_event', onclick)
+    cid = fig.canvas.mpl_connect('button_press_event', onclick)
+    
+    display(ev)
     
 def onclick(event):
     global valor_x, valor_y
-    x=int(event.xdata)
-    y=int(event.ydata)
-    print(x,y)
+    valor_x.value=int(event.xdata)
+    valor_y.value=int(event.ydata)
     
 def clear():
     clear_output()
@@ -230,6 +287,41 @@ def date_on_change(v):
     propiedades[1]=v['new']
     actualiza_layout()
     
+    
+def muestra_ev_prof():
+    fig3= plt.figure()
+    fig3.add_subplot()
+    eje_y=[i for i in range(34)]
+    eje_x=[dataset.variables[lista[0][propiedades[0]]][propiedades[1],i,valor_x.value, valor_y.value] for i in range(34)]
+    
+    plt.plot(eje_x,eje_y)
+    plt.title(lista[0][propiedades[0]])
+    plt.xlabel("profundidad")
+    
+def muestra_ev_tiempo():
+    fig3= plt.figure()
+    fig3.add_subplot()
+    
+    eje_x=[date[i] for i in range(len(dataset.variables[time])-1)]
+    if lista[1][propiedades[0]]==4:
+        eje_y=[dataset.variables[lista[0][propiedades[0]]][i,propiedades[2],valor_x.value, valor_y.value] for i in range(dataset.dimensions[time].size -1)]
+    
+    if lista[1][propiedades[0]]==3:
+        eje_y=[dataset.variables[lista[0][propiedades[0]]][i,valor_x.value, valor_y.value] for i in range(dataset.dimensions[time].size -1)]
+    
+    plt.plot(eje_x,eje_y)
+    plt.title(lista[0][propiedades[0]])
+    plt.xlabel("time")
+    
+def on_button_clicked_ev_prof(b):
+    actualiza_layout()
+    muestra_ev_prof()
+
+
+def on_button_clicked_ev_time(b):
+    actualiza_layout()
+    muestra_ev_tiempo()
+
 
     
 #Menu
